76f1dba6b47a5758642bb043e782172784137aa4
Arpitadey88 <deyarpitabanik@gmail.com>
2022-07-16 10:44:42 +0600

    added admin section

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middlewars
-app.use(cors());
-app.use(express.json());
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lxlvg.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// middlewars
+app.use(cors());
+app.use(express.json());
 
 async function run() {
   try {
@@ -24,6 +24,7 @@ async function run() {
     const database = client.db('eComShop');
     const productsCollection = database.collection('products');
     const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection('users');
 
     // POST/Insert Product to Database from json
     app.post('/addProducts', async (req, res) => {
@@ -58,7 +59,7 @@ async function run() {
       });
       res.json(result);
     });
-    
+
     // add order
     app.post("/addOrder", async (req, res) => {
       console.log(req.body);
@@ -94,7 +95,6 @@ async function run() {
     });
 
     // update-order-status
-
     app.put("/orderStatusUpdate", async (req, res) => {
       const id = req?.body?.id;
       const status = req?.body?.status;
@@ -107,8 +107,47 @@ async function run() {
       const result = await ordersCollection.updateOne(query, updateDoc);
       res.json(result);
     });
-  }
 
+    // Add Save Users
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    // Make Users
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    // Make Admin
+    app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+      console.log('put', user);
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: 'admin' } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    })
+
+    // Check Admin or Not
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    })
+
+  }
   finally {
     // await client.close();
   }
